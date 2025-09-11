90eaff82383e1f2ba0293694aed439961e82c040
Kritarth Srivastava <oftheleaf.hidden@gmail.com>
2025-09-11 17:45:18 +0530

    Initialized DVC

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a fraud detection model using Python and scikit-learn to classify credit card transactions as fraudulent or legitimate, achieving X% accuracy.',
+    author='Kritarth Srivastava',
+    license='',
+)
